d86c30c905a8faa022deec878cc224be767b6112
Rashed Almanasrah <rashed.almanasah@yahoo.com>
2025-03-24 07:49:51 +0300

    token instead server session

--- src/app/api/daily-video/questions/route.ts
+++ src/app/api/daily-video/questions/route.ts
@@ -1,21 +1,20 @@
-import { NextResponse } from "next/server";
-import { getServerSession } from "next-auth";
+import { NextResponse, NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
 import pool from "@/lib/db";
-import { authOptions } from "@/lib/nextAuth";
 
-export async function POST(request: Request) {
-  const session = await getServerSession(authOptions);
+export async function POST(request: NextRequest) {
+  const token = await getToken({ req: request });
 
-  if (!session) {
+  if (!token) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
   const { content } = await request.json();
-  console.log(content, session);
+  console.log(content, token);
   const client = await pool.connect();
   try {
     const query = "INSERT INTO questions (user_id, content) VALUES ($1, $2)";
-    await client.query(query, [session.user.id, content]);
+    await client.query(query, [token.id, content]);
 
     return NextResponse.json({ message: "Question added" }, { status: 201 });
   } catch (error) {
@@ -26,24 +25,17 @@ export async function POST(request: Request) {
   }
 }
 
-export async function GET() {
-  const session = await getServerSession();
+export async function GET(request: NextRequest) {
+  const token = await getToken({ req: request });
 
-  if (!session) {
-    return new NextResponse("Unauthorized", { status: 401 });
+  if (!token) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
-  if (session) {
-    if (session.user) {
-      if (session.user.email) {
-        const authorizedEmails =
-          process.env.AUTHORIZED_EMAILS?.split(",") || [];
-        if (!authorizedEmails.includes(session.user.email)) {
-          return new NextResponse("Unauthorized", { status: 401 });
-        }
-      } else {
-        return new NextResponse("Unauthorized", { status: 401 });
-      }
-    }
+
+  // Check authorized emails
+  const authorizedEmails = process.env.AUTHORIZED_EMAILS?.split(",") || [];
+  if (!authorizedEmails.includes(token.email as string)) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
   const client = await pool.connect();
@@ -59,7 +51,6 @@ export async function GET() {
     return NextResponse.json(result.rows, { status: 200 });
   } catch (error) {
     console.log(error);
-
     return NextResponse.json({ error: "Server error" }, { status: 500 });
   } finally {
     client.release();
